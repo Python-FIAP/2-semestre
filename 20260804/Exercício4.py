fa56b41303b65038db44263df89cee3ce543f03a
Eduardo Bechara Medeiros Craveiro <eduardo.craveiro26@gmail.com>
2026-08-11 17:13:44 -0300

    Add media function to calculate average of three numbers
    Implement function to calculate arithmetic mean of three numbers.

---
+++ 20260804/Exercício4.py
@@ -0,0 +1,7 @@
+#Implemente a função media, que recebe três valores numéricos e
+#retorna a média aritmética dos valores.
+
+#def media(num1, num2, num3):
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
