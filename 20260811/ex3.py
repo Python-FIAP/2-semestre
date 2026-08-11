7cfbeb90cdc2dd974b781d236f83bf32c280d596
Eduardo Bechara Medeiros Craveiro <eduardo.craveiro26@gmail.com>
2026-08-11 17:14:32 -0300

    Add soma function to sum two numbers

---
+++ 20260811/ex3.py
@@ -0,0 +1,10 @@
+def soma(a:float = 0, b:float = 0) -> float:
+    """
+    Soma de dois numeros
+    :param a: primeira parcela
+    :param b: segunda parcela
+    :return: total
+    """
+    return a+b
+
+print(soma(2, 3))
